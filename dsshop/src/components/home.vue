931714262d2ae5be311594295a074b7db5b4b2ca
thn007 <1759205852@qq.com>
2019-01-02 15:34:42 +0800

    完成了退出功能和验证信息

--- dsshop/src/components/home.vue
+++ dsshop/src/components/home.vue
@@ -9,7 +9,7 @@
                                     <h2>电商后台管理系统</h2>
                                 </el-col>
                                 <el-col :span="1">
-                                    <a href="" class="loginout">退出</a>
+                                    <a href="" class="loginout" @click.prevent="loginout()">退出</a>
                                 </el-col>
                         </el-row>
                 </el-header>
@@ -19,11 +19,11 @@
                         <el-submenu index="1">
                           <template slot="title">
                             <i class="el-icon-location"></i>
-                            <span>导航一</span>
+                            <span>用户管理</span>
                           </template>
                           <el-menu-item index="1-1">
                             <i class="el-icon-location"></i>
-                            选项1
+                            用户列表
                           </el-menu-item>
                         </el-submenu>
                         <el-submenu index="2">
@@ -66,7 +66,27 @@
 </template>
 <script>
 export default{
-
+  // 权限验证
+  beforeCreate(){
+    if(!localStorage.getItem("token")){
+      this.$router.push({
+        name:'login'
+      })
+      this.$message.warning('请先登录')
+    }
+  },
+  methods:{
+    loginout(){
+      // 1.清楚token
+     localStorage.clear();
+      // 2.退出到登录页
+      this.$router.push({
+        name :'login'
+      })
+      // 3.提示退出成功
+      this.$message.success('退出成功')
+    }
+  }
 }
 </script>
 <style>
